07d7066469c05d091492625a7bae414a09e7d107
SHAKIB AHMMED <shakibahmmed1@gmail.com>
2025-11-11 00:09:20 +0600

    Bug Fixed

--- src/Pages/AllProperties.jsx
+++ src/Pages/AllProperties.jsx
@@ -9,9 +9,9 @@ const AllProperties = () => {
     const { user } = useContext(AuthContext);
 
     return (
-        <div className="p-4 pt-5 flex flex-col justify-center">
+        <div className="p-4 bg-gray-100 pt-5 flex flex-col justify-center">
             <h1 className="flex justify-center pt-5 pb-5 text-3xl font-bold mb-6">All Properties</h1>
-            <div className="grid grid-cols-1 pb-5 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 pb-5 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                 {properties.map((property) => (
                     <PropertyCard key={property._id} property={property} user={user} />
                 ))}
--- src/Pages/PropertyCard.jsx
+++ src/Pages/PropertyCard.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
-const PropertyCard = ({ property, user }) => {
+const PropertyCard = ({ property }) => {
     const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
 
     const handleViewDetails = () => {
         if (!user) {
-            navigate("login");
+            navigate("/login");
             return;
         }
-        navigate(`/property/${property._id}`);
+        navigate(`/properties-details/${property._id}`);
     };
 
     return (
@@ -19,29 +21,21 @@ const PropertyCard = ({ property, user }) => {
                 alt={property.PropertyName}
                 className="h-48 w-full object-cover"
             />
-
             <div className="p-4 flex flex-col flex-grow">
-
                 <h2 className="text-xl font-bold mb-1">{property.PropertyName}</h2>
-
                 <p className="text-m font-semibold text-gray-600 mb-2">{property.Category}</p>
-                <p className="text-gray-700 mb-2">
-                    {property.Description.length > 100
-                        ? property.Description.slice(0, 100) + "..."
-                        : property.Description}
-                </p>
+                <p className="text-gray-700 mb-2">{property.Description}</p>
                 <p className="text-gray-500 text-m font-bold mb-2">{property.Location}</p>
                 <p className="text-[#075a12] font-semibold text-lg mb-4">
                     ${property.Price.toLocaleString()}
                 </p>
             </div>
-
-            <Link
-                to={`/properties-details/${property.propertyId}`}
-                className="t-auto text-center bg-[#075a12] hover:bg-green-900 text-white font-semibold hover:scale-105 transition ease-in-out py-2 px-4 rounded"
+            <button
+                onClick={handleViewDetails}
+                className="text-center bg-[#075a12] hover:bg-green-950 text-white font-semibold hover:scale-105 transition ease-in-out py-2 px-4 rounded"
             >
                 View Details
-            </Link>
+            </button>
         </div>
     );
 };
